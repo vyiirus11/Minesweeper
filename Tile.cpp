8dcaa10b09365409ec2c50aa8ce3920628fa8432
vyiirus11 <113479165+vyiirus11@users.noreply.github.com>
2023-03-11 04:04:19 -0500

    Tiles

---
+++ Tile.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+#include "Tile.h"
+
+// Constructor
+Tile::Tile(sf::Texture &texture, sf::Texture &flagTexture) {
+    sprite1->setTexture(texture);
+    sprite2->setTexture(texture);
+    flag->setTexture(flagTexture);
+}
+
+// Accessors
+Sprite* Tile::getSprite1() {
+    return sprite1;
+}
+
+Sprite* Tile::getSprite2() {
+    return sprite2;
+}
+
+Sprite* Tile::getFlagSprite() {
+    return flag;
+}
+
+bool Tile::getIsBomb() {
+    return isBomb;
+}
+
+bool Tile::getIsDebug() {
+    return debug;
+}
+
+bool Tile::getIsFlagged() {
+    return isFlagged;
+}
+
+int Tile::getAdjacentBombs() {
+    return adjacentBombs;
+}
+
+vector<Tile*>* Tile::getNeighbors() {
+    return &neighbors;
+}
+
+// Mutators
+void Tile::setNeighbors(vector<Tile*> &neighborTiles) {
+    neighbors = neighborTiles;
+    adjacentBombs = 0;
+
+    for (int i = 0; i < neighbors.size(); i++) {
+        Tile* current = neighbors.at(i);
+        if (current->getIsBomb()) {
+            adjacentBombs++;
+        }
+    }
+}
+
+void Tile::setIsBomb(bool change) {
+    isBomb = change;
+}
+
+void Tile::setIsDebug(bool change) {
+    debug = change;
+}
+
+void Tile::setIsFlagged(bool change) {
+    isFlagged = change;
+}
+
+// Functions
+void Tile::move(float x, float y) {
+    sprite1->move(Vector2f(y, x));
+    sprite2->move(Vector2f(y, x));
+    flag->move(Vector2f(y, x));
+}
+
+void Tile::makeBomb() {
+    isBomb = true;
+}
--- Tile.h
+++ Tile.h
@@ -1,8 +1,44 @@
-//
-// Created by bluea on 3/11/2023.
-//
+#pragma once
+#include <vector>
+#include <SFML/Graphics.hpp>
+using namespace std;
+using namespace sf;
 
-#ifndef MINESWEEPER_TILE_H
-#define MINESWEEPER_TILE_H
+class Tile
+{
+    bool debug = false;
+    bool isBomb = false;
+    bool isFlagged = false;
+    int adjacentBombs = 0;
+    vector<Tile*> neighbors;
 
-#endif //MINESWEEPER_TILE_H
+    Sprite* sprite1 = new Sprite;
+    Sprite* sprite2 = new Sprite;
+    Sprite* flag = new Sprite;
+
+public:
+    // Tile Constructor
+    Tile(Texture& texture, Texture& flagTexture);
+
+    // Accessors
+    Sprite* getSprite1();
+    Sprite* getSprite2();
+    Sprite* getFlagSprite();
+    bool getIsBomb();
+    bool getIsDebug();
+    bool getIsFlagged();
+    int getAdjacentBombs();
+    vector<Tile*>* getNeighbors();
+
+    // Mutators
+    void setNeighbors(vector<Tile*> &neighborTiles);
+    void setIsBomb(bool change);
+    void setIsDebug(bool change);
+    void setIsFlagged(bool change);
+
+    // Functions
+    void move(float x, float y);
+    void makeBomb();
+};
+
+};
